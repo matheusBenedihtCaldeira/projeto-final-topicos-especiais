74ad87567750713a1e79c20b028ec726036ffbe8
Matheus Benediht <matheusbenediht@gmail.com>
2024-06-18 21:51:51 -0300

    Update: Senha

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '62312898',
+    //TROCAR SENHA
+    password: '',
     database: 'db_projeto_dalva'
 });
 
